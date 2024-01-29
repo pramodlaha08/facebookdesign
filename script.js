5340d0bd651162585bcf0161c927d72f62964fde
pramodlaha08 <153620467+pramodlaha08@users.noreply.github.com>
2024-01-29 18:34:10 +0545

    Facebook design

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+/*
+
+function getViewportSize() {
+    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    return {width: width, height: height};
+}
+
+var viewportSize = getViewportSize();
+alert("Width: " + viewportSize.width + ", Height: " + viewportSize.height);
+*/
+
+/* Date */
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = today.toLocaleString('default', { month: 'long' }); // get month name
+var yyyy = today.getFullYear();
+
+today = mm + ' ' + dd + ', ' + yyyy;
+document.getElementById("date").innerHTML = "Today is " + today;
+
+/* To set button location */
+
+let startBtn = document.getElementById("start_btn").onclick = function () {
+    location.href = "login.html";
+}
